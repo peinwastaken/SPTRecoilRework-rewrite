e9734078bf8adce1447673a67498bb73b7574448
peinwastaken <legokid173@gmail.com>
2026-07-17 09:56:25 +0300

    add vibeslopped build stuff, thx gippity

--- bundle-release.bat
+++ bundle-release.bat
@@ -1,9 +1,34 @@
 @echo off
-color b
+setlocal
 
-set outFile=%1
-set releaseDir=%2
+set "rootDir=%~dp0"
+set "releaseDir=%rootDir%Release"
+set "outFile=%rootDir%RecoilRework.7z"
+set "sevenZip=%ProgramFiles%\7-Zip\7z.exe"
 
-"%ProgramFiles%\7-Zip\7z.exe" a -t7z "%outFile%" "%releaseDir%\*"
+if not exist "%sevenZip%" set "sevenZip=%ProgramFiles(x86)%\7-Zip\7z.exe"
 
-echo Release bundled!
+if not exist "%sevenZip%" (
+    echo 7-Zip was not found. Install it or update the sevenZip path in this script.
+    exit /b 1
+)
+
+if exist "%releaseDir%" rmdir /s /q "%releaseDir%"
+if exist "%outFile%" del /f /q "%outFile%"
+
+dotnet build "%rootDir%RecoilReworkClient\RecoilReworkClient.csproj" --configuration Release --no-restore
+if errorlevel 1 goto :failure
+
+dotnet build "%rootDir%RecoilReworkServer\RecoilReworkServer.csproj" --configuration Release --no-restore
+if errorlevel 1 goto :failure
+
+"%sevenZip%" a -t7z "%outFile%" "%releaseDir%\*"
+if errorlevel 1 goto :failure
+
+echo Release bundled: "%outFile%"
+echo Release folder: "%releaseDir%"
+exit /b 0
+
+:failure
+echo Release build failed.
+exit /b 1
